14c4d51677a54a45cb0234a02436756197539e4f
Oleg Melnik <oleg.mel123@gmail.com>
2025-11-23 15:04:35 +0200

    Add result

--- src/app/features/qr-scanner/qr-scanner.ts
+++ src/app/features/qr-scanner/qr-scanner.ts
@@ -1,21 +1,36 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Signal, viewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  signal,
+  Signal,
+  viewChild, WritableSignal
+} from '@angular/core';
 import QrScanner from 'qr-scanner';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-qr-scanner',
   templateUrl: './qr-scanner.html',
   styleUrl: './qr-scanner.scss',
+  imports: [
+    JsonPipe
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class QrScannerComponent implements AfterViewInit {
 
   videoElem: Signal<ElementRef<HTMLVideoElement> | undefined> = viewChild<ElementRef<HTMLVideoElement>>('video');
   qrScanner!: QrScanner;
+  protected result: WritableSignal<any> = signal({});
 
   ngAfterViewInit(): void {
     this.qrScanner = new QrScanner(
       this.videoElem()!.nativeElement,
-      result => console.log('decoded qr code:', result),
+      result => {
+        this.result.set(result);
+      },
       { /* your options or returnDetailedScanResult: true if you're not specifying any other options */ },
     );
 
