1c54e4acd64977081768fa0fcab29fe3bfe8eacf
Oleg Melnik <oleg.mel123@gmail.com>
2026-01-03 15:00:45 +0200

    Change qr history

--- src/app/features/qr-history/qr-history.ts
+++ src/app/features/qr-history/qr-history.ts
@@ -7,10 +7,18 @@ import { EditListButton } from '../../core/components/edit-list-button/edit-list
   templateUrl: './qr-history.html',
   styleUrl: './qr-history.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.expanded]': 'isExpanded()'
+  },
   imports: [
     EditListButton
   ],
 })
 export class QrHistory {
   protected qrCodes = signal(inject(LocalQrStorageService).getQrCodes());
+  protected isExpanded = signal(false);
+
+  protected toggle(): void {
+    this.isExpanded.update(curr => !curr);
+  }
 }
